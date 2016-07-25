747e390ae2417b9c3abd6dca37d4d49e29812503
Hexvane <gchougland@gmail.com>
2016-07-25 12:15:46 -0500

    menu stuff

--- src/main.h
+++ src/main.h
@@ -1,3 +1,11 @@
 #pragma once
+
+typedef struct {
+	char question_text[1][24];
+	char watch_token[1][30];
+	char answers[4][48];
+	uint8_t amount_of_answers;
+} Question;
+
 void menu_cfg_provider(void *context);
 void setup_menu_layer(Window *questions);
